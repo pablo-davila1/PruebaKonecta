a60201c4f6b4e19ce65284215f793c3a08763a0e
pablo-davila1 <62685470+pablo-davila1@users.noreply.github.com>
2023-01-04 13:24:16 -0500

    ADD project

---
+++ PruebaKonecta/src/main/java/com/Konecta/PruebaKonecta/PruebaKonectaApplication.java
@@ -0,0 +1,13 @@
+package com.Konecta.PruebaKonecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaKonectaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaKonectaApplication.class, args);
+	}
+
+}
